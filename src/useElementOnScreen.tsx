21c3290c5dbe526a6ff95f55a54bec63834e086a
Ioannis Retsas <joretsas@gmail.com>
2023-03-31 14:54:30 +0100

    [master]: init

--- src/useElementOnScreen.tsx
+++ src/useElementOnScreen.tsx
@@ -1,23 +1,22 @@
-import { useState, useEffect, useRef} from "react";
+import { useState, useEffect, useRef } from "react";
 
 export const useElementOnScreen = (options: any) => {
-    const [isVisible, setIsVisible] = useState(false);
-    const containerRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+  const containerRef = useRef(null);
 
-    const callbackFunction = (entries: any) => {
-      const [entry] = entries;
-      setIsVisible(entry.isIntersecting)
-    }
-
-    useEffect(() => {
-      const observer = new IntersectionObserver(callbackFunction, options);
-      if (containerRef.current) observer.observe(containerRef.current);
-
-      return () => {
-        if (containerRef.current) observer.unobserve(containerRef.current)
-      }
-    }, [containerRef, isVisible]);
-
-    return [containerRef, isVisible];
+  const callbackFunction = (entries: any) => {
+    const [entry] = entries;
+    setIsVisible(entry.isIntersecting)
   }
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callbackFunction, options);
+    if (containerRef.current) observer.observe(containerRef.current);
+
+    return () => {
+      if (containerRef.current) observer.unobserve(containerRef.current)
+    }
+  }, [containerRef, isVisible]);
+
+  return [containerRef, isVisible];
 }
